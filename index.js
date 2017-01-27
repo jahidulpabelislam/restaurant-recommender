5f4d38547453178ed68037334f2409a51780247c
Ade Crown <adecrown1@live.co.uk>
2017-01-27 18:54:59 +0000

    added app.post

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require("express"),
-    app = express(),
-    http = require("http"),
-    server = http.Server(app);
+app = express(),
+http = require("http"),
+server = http.Server(app);
 
 server.listen(9000);
 
 app.use(express.static(__dirname + "/public"));
-
+app.use(express.static(__dirname + "/js"));
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
@@ -14,78 +14,82 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 var Yelp = require('yelp');
 
 var yelp = new Yelp({
-    consumer_key: 'NYLIYvAuI7kb917AbxGG7g',
-    consumer_secret: 'Fvm2IdcYp3S0pD19Vducgu9Pabs',
-    token: 'qb7kTYEd0RbbqKnYxsNXyzS2ER1RAEA4',
-    token_secret: '4cr6oFU8197yUKEtovdrcMhEJwM'
+  consumer_key: 'NYLIYvAuI7kb917AbxGG7g',
+  consumer_secret: 'Fvm2IdcYp3S0pD19Vducgu9Pabs',
+  token: 'qb7kTYEd0RbbqKnYxsNXyzS2ER1RAEA4',
+  token_secret: '4cr6oFU8197yUKEtovdrcMhEJwM'
+});
+
+app.post('/recommendations/', function(req, res) {
+  
+  console.log(req.body);
 });
 
 app.get("/recommendations/", function (req, res) {
 
-    /*yelp.search({term: 'food', location: 'Portsmouth'})
-     .then(function (data) {
-     res.send(JSON.stringify(response));
-     })
-     .catch(function (err) {
-     res.send(JSON.stringify(response));
-     });*/
+  /*yelp.search({term: 'food', location: 'Portsmouth'})
+  .then(function (data) {
+  res.send(JSON.stringify(response));
+})
+.catch(function (err) {
+res.send(JSON.stringify(response));
+});*/
+var request = require('request');
 
-    var request = require('request');
+request('https://maps.googleapis.com/maps/api/geocode/json?address=po11aq&key=AIzaSyDTY9OxJDd4_N2nVaNtdJng-YZcFYgmpEE', function (error, response, body) {
+  console.log('Status Code: ' + response.statusCode);
 
-    request('https://maps.googleapis.com/maps/api/geocode/json?address=po11aq&key=AIzaSyDTY9OxJDd4_N2nVaNtdJng-YZcFYgmpEE', function (error, response, body) {
-        console.log('Status Code: ' + response.statusCode);
+  if (error || response.statusCode != 200) {
+    console.log("Non 200 Response");
+  }
 
-        if (error || response.statusCode != 200) {
-            console.log("Non 200 Response");
-        }
+  if (!error && response.statusCode == 200) {
+    var locationResult = JSON.parse(body);
+    var lat =  locationResult.results[0].geometry.location.lat,
+    lng = locationResult.results[0].geometry.location.lng;
 
-        if (!error && response.statusCode == 200) {
-            var locationResult = JSON.parse(body);
-            var lat =  locationResult.results[0].geometry.location.lat,
-                lng = locationResult.results[0].geometry.location.lng;
+    request("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + lng + "&radius=5000&type=restaurant&key=AIzaSyAk--L5B1sdS9Q5TKi23t7a4jmqN30Z7bg", function (error, response, body) {
+      console.log('Status Code: ' + response.statusCode);
 
-            request("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + lng + "&radius=5000&type=restaurant&key=AIzaSyAk--L5B1sdS9Q5TKi23t7a4jmqN30Z7bg", function (error, response, body) {
-                console.log('Status Code: ' + response.statusCode);
+      if (error || response.statusCode != 200) {
+        console.log("Non 200 Response");
+      }
 
-                if (error || response.statusCode != 200) {
-                    console.log("Non 200 Response");
-                }
+      if (!error && response.statusCode == 200) {
+        var placesResult = JSON.parse(body),
+        restaurants = [];
 
-                if (!error && response.statusCode == 200) {
-                    var placesResult = JSON.parse(body),
-                        restaurants = [];
+        placesResult.results.forEach(function(restaurant) {
 
-                    placesResult.results.forEach(function(restaurant) {
+          request("https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyAk--L5B1sdS9Q5TKi23t7a4jmqN30Z7bg&placeid=" + restaurant.place_id, function (error, response, body) {
 
-                        request("https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyAk--L5B1sdS9Q5TKi23t7a4jmqN30Z7bg&placeid=" + restaurant.place_id, function (error, response, body) {
+            console.log('Status Code: ' + response.statusCode);
 
-                            console.log('Status Code: ' + response.statusCode);
+            if (error || response.statusCode != 200) {
+              console.log("Non 200 Response");
+            }
 
-                            if (error || response.statusCode != 200) {
-                                console.log("Non 200 Response");
-                            }
+            if (!error && response.statusCode == 200) {
+              restaurants.push(JSON.parse(body));
+              console.log("getting request");
+            }
 
-                            if (!error && response.statusCode == 200) {
-                                restaurants.push(JSON.parse(body));
-                                console.log("getting request");
-                            }
+            if (restaurants.length === 5) {
+              console.log("done");
+              res.send(restaurants);
+            }
 
-                            if (restaurants.length === 5) {
-                                console.log("done");
-                                res.send(restaurants);
-                            }
-
-                        });
-                    });
-                }
-            });
-
-        }
+          });
+        });
+      }
     });
+
+  }
+});
 });
--- js/recond.js
+++ js/recond.js
@@ -18,28 +18,29 @@ document.getElementById('done').onclick = function()
   }
   //console.log(chosenType);
 
+  
+  //// Post code
+  var pCode = document.getElementById('pCode').value;
+  var dist = document.getElementById('distance').value;
+  var service = document.getElementById('service').value;
 
-//// Post code
- var pCode = document.getElementById('pCode').value;
- var dist = document.getElementById('distance').value;
- var service = document.getElementById('service').value;
+  var myObj = { "postcode":pCode, "food":chosenType, "distance":dist, "service":service };
+  var myJSON = JSON.stringify(myObj);
+  console.log(myObj);
 
-var myObj = { "postcode":pCode, "food":chosenType, "distance":dist, "service":service };
-var myJSON = JSON.stringify(myObj);
-console.log(myObj);
+  var http = new XMLHttpRequest();
+  var url = "recommendations";
+  http.open("GET", url, true);
 
-var http = new XMLHttpRequest();
-var url = "/recommendations";
-http.open("POST", url, true);
+  //Send the proper header information along with the request
+  http.setRequestHeader("Content-type", "application/json");
 
-//Send the proper header information along with the request
-http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-http.onreadystatechange = function() {//Call a function when the state changes.
+  http.onreadystatechange = function() {//Call a function when the state changes.
     if(http.readyState == 4 && http.status == 200) {
-        alert(http.responseText);
+      alert(http.responseText);
     }
-}
-http.send(myObj);
+  }
+  http.send(myJSON);
+
 
 }
