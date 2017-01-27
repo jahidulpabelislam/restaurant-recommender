f48cb4a96420db20738ae0321af841e2537e24b5
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2017-01-27 23:28:16 +0000

    Only recommend if open at time.

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
-});
-
 var yelp = new Yelp({
     consumer_key: 'NYLIYvAuI7kb917AbxGG7g',
     consumer_secret: 'Fvm2IdcYp3S0pD19Vducgu9Pabs',
     token: 'qb7kTYEd0RbbqKnYxsNXyzS2ER1RAEA4',
     token_secret: '4cr6oFU8197yUKEtovdrcMhEJwM'
+});
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.post('/recommendations/', function (req, res) {
@@ -40,6 +40,8 @@ app.post('/recommendations/', function (req, res) {
                 return a.rating - b.rating;
             });
 
+            var count = 0;
+
             restaurants.forEach(function (restaurant) {
 
                 var lat = restaurant.location.coordinate.latitude;
@@ -53,13 +55,22 @@ app.post('/recommendations/', function (req, res) {
 
                         placesResult.results.forEach(function (restaurant) {
 
-                            request("https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyCdKWpGk2NqT_Mdx0L7oudzR8mdLQ0KTYk&placeid=" + restaurant.place_id, function (error, response, body) {
+                            request("https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyCdKWpGk2NqT_Mdx0L7oudzR8mdLQ0KTYk&placeid=" + restaurant.place_id, function (error, response, body2) {
+
+                                count++;
 
                                 if (!error && response.statusCode == 200) {
-                                    restaurantsRecommended.push(JSON.parse(body).result);
+                                    var day = 6;
+                                    var time = 1700;
+                                    var data = JSON.parse(body2).result;
+                                    var openingHours = data.opening_hours;
+
+                                    if (openingHours && openingHours.periods[day] && parseInt(openingHours.periods[day].open.time) <= time && parseInt(openingHours.periods[day].close.time) >= time) {
+                                        restaurantsRecommended.push(data);
+                                    }
                                 }
 
-                                if (restaurantsRecommended.length === 5) {
+                                if (restaurantsRecommended.length === 5 || count >= restaurants.length) {
                                     responseData.data = restaurantsRecommended;
                                     res.send(responseData);
                                 }
@@ -70,7 +81,7 @@ app.post('/recommendations/', function (req, res) {
                 });
             });
         })
-        .catch(function (err) {
+        .catch(function () {
             responseData.error = "Error";
             res.send(responseData);
         });
