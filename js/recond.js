4ebbdbe180218e672005e2dd8b550bdf7f1e7f06
Ade Crown <adecrown1@live.co.uk>
2017-01-27 17:05:07 +0000

    html

--- js/recond.js
+++ js/recond.js
@@ -27,4 +27,19 @@ document.getElementById('done').onclick = function()
 var myObj = { "postcode":pCode, "food":chosenType, "distance":dist, "service":service };
 var myJSON = JSON.stringify(myObj);
 console.log(myObj);
+
+var http = new XMLHttpRequest();
+var url = "/recommendations";
+http.open("POST", url, true);
+
+//Send the proper header information along with the request
+http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+http.onreadystatechange = function() {//Call a function when the state changes.
+    if(http.readyState == 4 && http.status == 200) {
+        alert(http.responseText);
+    }
+}
+http.send(myObj);
+
 }
