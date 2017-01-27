b3727ccb7dd9964c983c33a109859f8e60a1698a
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2017-01-27 13:52:10 +0000

    Use Yelp & Send Random Recommendations.

--- index.js
+++ index.js
@@ -13,9 +13,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res) {
-    res.sendFile("/index.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/recommendations/", function (req, res) {
+var Yelp = require('yelp');
 
+var yelp = new Yelp({
+    consumer_key: 'NYLIYvAuI7kb917AbxGG7g',
+    consumer_secret: 'Fvm2IdcYp3S0pD19Vducgu9Pabs',
+    token: 'qb7kTYEd0RbbqKnYxsNXyzS2ER1RAEA4',
+    token_secret: '4cr6oFU8197yUKEtovdrcMhEJwM'
+});
+
+
+app.get("/recommendations/", function (req, res) {
+        yelp.search({ term: 'food', location: 'Portsmouth' })
+            .then(function (data) {
+                res.send(JSON.stringify(response));
+            })
+            .catch(function (err) {
+                res.send(JSON.stringify(response));
+            });
 });
