266675660b90064e92696b78fdab42100a462bf4
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2017-01-29 21:42:21 +0000

    Code Cleanup

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-var express = require("express"),
+const express = require("express"),
     app = express(),
-    http = require("http"),
-    server = http.Server(app),
     request = require('request'),
     Yelp = require('yelp'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    yelp = new Yelp({
+        consumer_key: "NYLIYvAuI7kb917AbxGG7g",
+        consumer_secret: "Fvm2IdcYp3S0pD19Vducgu9Pabs",
+        token: "Qqwq-Dr1yaEywOfQD7HjRvM02JlV4o64",
+        token_secret: "hZnaVn2TFa1VJs-Xu37r-G1gDOo"
+    });
 
-server.listen(9000);
+require("http").Server(app).listen(9000);
 
 app.use(express.static(__dirname + "/js"));
 
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({extended: true}));
-
-var yelp = new Yelp({
-    consumer_key: 'NYLIYvAuI7kb917AbxGG7g',
-    consumer_secret: 'Fvm2IdcYp3S0pD19Vducgu9Pabs',
-    token: 'qb7kTYEd0RbbqKnYxsNXyzS2ER1RAEA4',
-    token_secret: '4cr6oFU8197yUKEtovdrcMhEJwM'
-});
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
@@ -29,7 +26,7 @@ app.post('/recommendations/', function (req, res) {
 
     var responseData = {};
 
-    yelp.search({term: 'food', location: req.body.postcode, sort: 2, radius_filter: req.body.distance, category_filter: req.body.food.join()})
+    yelp.search({term: "food", location: req.body.postcode, sort: 2, radius_filter: parseInt(req.body.distance) * 1000, category_filter: "french,italian" })
         .then(function (data) {
 
             var restaurantsRecommended = [];
@@ -50,16 +47,16 @@ app.post('/recommendations/', function (req, res) {
 
                 request("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + long + "&keyword=" + name + "&radius=10&type=restaurant&key=AIzaSyCdKWpGk2NqT_Mdx0L7oudzR8mdLQ0KTYk", function (error, response, body) {
 
-                    if (!error && response.statusCode == 200) {
+                    count++;
+
+                    if (!error && response.statusCode === 200) {
                         var placesResult = JSON.parse(body);
 
                         placesResult.results.forEach(function (restaurant) {
 
                             request("https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyCdKWpGk2NqT_Mdx0L7oudzR8mdLQ0KTYk&placeid=" + restaurant.place_id, function (error, response, body2) {
 
-                                count++;
-
-                                if (!error && response.statusCode == 200) {
+                                if (!error && response.statusCode === 200) {
                                     var day = 6,
                                         time = 1700,
                                         data = JSON.parse(body2).result,
@@ -71,7 +68,7 @@ app.post('/recommendations/', function (req, res) {
                                 }
 
                                 if (restaurantsRecommended.length === 5 || count >= restaurants.length) {
-                                    responseData.data = restaurantsRecommended;
+                                    responseData.restaurantsRecommended = restaurantsRecommended;
                                     res.send(responseData);
                                 }
 
