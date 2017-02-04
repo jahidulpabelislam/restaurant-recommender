5d6eb676dafd9b40188ea9e5fef35f22f3fd1457
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2017-02-04 19:42:03 +0000

    Make the look of the app better.

--- js/recommend.js
+++ js/recommend.js
@@ -39,7 +39,10 @@ document.getElementById('preferences').onsubmit = function () {
             if (data && data.restaurantsRecommended && data.restaurantsRecommended.length > 0) {
                 data.restaurantsRecommended.forEach(function (restaurant) {
                     var div = document.createElement("div");
-                    var name = document.createElement("p");
+                    var div2 = document.createElement("div");
+                    var div3 = document.createElement("div");
+                    var div4 = document.createElement("div");
+                    var name = document.createElement("h3");
                     var category = document.createElement("p");
                     var rating = document.createElement("p");
                     var address = document.createElement("p");
@@ -47,24 +50,36 @@ document.getElementById('preferences').onsubmit = function () {
                     var site = document.createElement("a");
 
                     div.className = "col-md-4";
-                    name.innerHTML = "Name: " + restaurant.name;
+                    name.innerHTML = restaurant.name;
+                    name.className = "panel-title";
                     address.innerHTML = "Address: " + restaurant.address;
                     number.innerHTML = "Phone Number: " + restaurant.phone;
                     rating.innerHTML = "Rating: " + restaurant.rating;
                     var cats = [];
+
                     restaurant.categories.forEach(function(cat) {
-                        cats+= cat[0];
+                        cats.push(cat[0]);
                     });
-                    category.innerHTML = "Categories: " + cats;
+
+                    category.innerHTML = "Categories: " + cats.join(", ");
                     site.innerHTML = restaurant.website;
                     site.href = restaurant.website;
 
-                    div.appendChild(name);
-                    div.appendChild(address);
-                    div.appendChild(number);
-                    div.appendChild(rating);
-                    div.appendChild(category);
-                    div.appendChild(site);
+                    div2.className = "panel panel-default";
+                    div3.className = "panel-heading";
+                    div4.className = "panel-body";
+
+                    div3.appendChild(name);
+                    div4.appendChild(address);
+                    div4.appendChild(number);
+                    div4.appendChild(rating);
+                    div4.appendChild(category);
+                    div4.appendChild(site);
+
+                    div2.appendChild(div3);
+                    div2.appendChild(div4);
+
+                    div.appendChild(div2);
 
                     recommendation.appendChild(div);
                 });
