2b825c6c4fe49327430349323daee86d2a527666
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2019-08-03 16:28:03 +0100

    Tidy up frontend JS

--- js/recommend.js
+++ js/recommend.js
@@ -1,24 +1,87 @@
+function renderRestaurants(res) {
+    var recommendationsElem = document.getElementById("recommendation");
+    recommendationsElem.innerHTML = "";
+
+    var recommendedRestaurants = res.recommendedRestaurants || [];
+    if (recommendedRestaurants.length) {
+        for (var i = 0; i < recommendedRestaurants.length; i++) {
+            var div = document.createElement("div");
+            var div2 = document.createElement("div");
+            var div3 = document.createElement("div");
+            var div4 = document.createElement("div");
+            var name = document.createElement("h3");
+            var category = document.createElement("p");
+            var rating = document.createElement("p");
+            var address = document.createElement("p");
+            var number = document.createElement("p");
+            var site = document.createElement("a");
+
+            div.className = "col-md-4";
+            name.innerHTML = restaurant.name;
+            name.className = "panel-title";
+            address.innerHTML = "Address: " + restaurant.address;
+            number.innerHTML = "Phone Number: " + restaurant.phone;
+            rating.innerHTML = "Rating: " + restaurant.rating;
+
+            var cats = [];
+            for (var i; i < restaurant.categories.length; i++) {
+                cats.push(cat[0]);
+            }
+
+            category.innerHTML = "Categories: " + cats.join(", ");
+            site.innerHTML = restaurant.website;
+            site.href = restaurant.website;
+
+            div2.className = "panel panel-default";
+            div3.className = "panel-heading";
+            div4.className = "panel-body";
+
+            div3.appendChild(name);
+            div4.appendChild(address);
+            div4.appendChild(number);
+            div4.appendChild(rating);
+            div4.appendChild(category);
+            div4.appendChild(site);
+
+            div2.appendChild(div3);
+            div2.appendChild(div4);
+
+            div.appendChild(div2);
+
+            recommendationsElem.appendChild(div);
+        }
+    }
+    else if (res && res.error) {
+        recommendationsElem.innerHTML = "<p>Error getting your recommendations, please try again later.</p>";
+    }
+    else {
+        recommendationsElem.innerHTML = "<p>No recommendations found for your preferences.</p>";
+    }
+}
+
 document.getElementById("preferences").onsubmit = function() {
 
-    //// Food Type
-    var foodType = document.getElementsByName("myCheck");
-    var chosenType = [];
-    var typeCount = 0;
+    var foodType = document.getElementsByName("foodType");
+    var chosenTypes = [];
 
-    while (typeCount < foodType.length) {
-        if (foodType[typeCount].checked) {
-            chosenType.push(foodType[typeCount].value);
+    for (var i = 0; i < foodType.length; i++) {
+        if (foodType[i].checked) {
+            chosenTypes.push(foodType[i].value);
         }
-        typeCount++;
     }
 
-    //// Post code
     var pCode = document.getElementById("pCode").value;
     var dist = document.getElementById("distance").value;
     var time = document.getElementById("time").value;
     var day = document.getElementById("day").value;
 
-    var preferences = {"postcode": pCode, "food": chosenType, "distance": dist, "time": time, "day": day};
+    var preferences = {
+        "postcode": pCode,
+        "food": chosenTypes,
+        "distance": dist,
+        "time": time,
+        "day": day,
+    };
     var preferencesJSON = JSON.stringify(preferences);
 
     var http = new XMLHttpRequest();
@@ -31,65 +94,8 @@ document.getElementById("preferences").onsubmit = function() {
     //Call a function when the state changes.
     http.onreadystatechange = function() {
         if (http.readyState === 4 && http.status === 200) {
-            const data = JSON.parse(http.responseText),
-                recommendation = document.getElementById("recommendation");
-
-            recommendation.innerHTML = "";
-
-            if (data && data.recommendedRestaurants && data.recommendedRestaurants.length > 0) {
-                data.recommendedRestaurants.forEach(function(restaurant) {
-                    var div = document.createElement("div");
-                    var div2 = document.createElement("div");
-                    var div3 = document.createElement("div");
-                    var div4 = document.createElement("div");
-                    var name = document.createElement("h3");
-                    var category = document.createElement("p");
-                    var rating = document.createElement("p");
-                    var address = document.createElement("p");
-                    var number = document.createElement("p");
-                    var site = document.createElement("a");
-
-                    div.className = "col-md-4";
-                    name.innerHTML = restaurant.name;
-                    name.className = "panel-title";
-                    address.innerHTML = "Address: " + restaurant.address;
-                    number.innerHTML = "Phone Number: " + restaurant.phone;
-                    rating.innerHTML = "Rating: " + restaurant.rating;
-                    var cats = [];
-
-                    restaurant.categories.forEach(function(cat) {
-                        cats.push(cat[0]);
-                    });
-
-                    category.innerHTML = "Categories: " + cats.join(", ");
-                    site.innerHTML = restaurant.website;
-                    site.href = restaurant.website;
-
-                    div2.className = "panel panel-default";
-                    div3.className = "panel-heading";
-                    div4.className = "panel-body";
-
-                    div3.appendChild(name);
-                    div4.appendChild(address);
-                    div4.appendChild(number);
-                    div4.appendChild(rating);
-                    div4.appendChild(category);
-                    div4.appendChild(site);
-
-                    div2.appendChild(div3);
-                    div2.appendChild(div4);
-
-                    div.appendChild(div2);
-
-                    recommendation.appendChild(div);
-                });
-            }
-            else if (data && data.error) {
-                recommendation.innerHTML = "<p>Error getting your recommendations, please try again later.</p>";
-            }
-            else {
-                recommendation.innerHTML = "<p>No recommendations found for your preferences.</p>";
-            }
+            var res = JSON.parse(http.responseText);
+            renderRestaurants(res);
         }
     };
 
